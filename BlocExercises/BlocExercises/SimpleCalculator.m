64c6946a23f57b17695602a467ac9bb7a28f7782
Alejandro <alejandroreiter@gmail.com>
2014-08-11 18:31:51 -0500

    Completed numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -10,19 +10,21 @@
 
 @implementation SimpleCalculator
 
-- (NSInteger) increaseNumberBy1:(NSInteger) number {
-    /* WORK HERE */
-    return number;
+
+- (NSInteger) increaseNumberBy1: (NSInteger) number {
+    /* Work Here */
+    return number + 1;
 }
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
-    /* WORK HERE */
-    return number1;
+    /* Work Here */
+//    [self addNumber:5 toNumber:5];
+    return number1 + number2;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    return dividend % divisor;
 }
 
 @end
