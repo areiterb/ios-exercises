51e93cc8fc5dac45cc709a6696024e4fc84991c0
Alejandro <alejandroreiter@gmail.com>
2014-08-12 18:46:04 -0500

    Completed equality

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,23 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    BOOL areTheyEqual = [string1 isEqualToString:string2];
+    NSLog(@"Are they equal? %@", areTheyEqual ? @"Yes" : @"No");
+    return [string1 isEqualToString:string2];
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    BOOL areTheyEqual = [number1 isEqualToNumber:number2];
+    NSLog(@"Are they equal? %@", areTheyEqual ? @"Yes" : @"No");
+    return [number1 isEqual:number2];
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    BOOL isGreaterThan = (integer1 > integer2);
+    NSLog(@"Is 7 greater than 4? %@", integer1 > integer2 ? @"Yes" : @"No");
+    return (integer1 > integer2);
 }
 
 @end
--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,17 +12,32 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    // NSString *fullSentence = [cheese favoriteCheeseStringWithCheese:@"cheddar"];
+    // fullSentence is "My favorite cheese is cheddar."
+    NSString *fullSentence = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    NSLog(fullSentence);
+    return fullSentence;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    NSString *cheeseNameOnly = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+    NSLog(@"%@", cheeseNameOnly);
+    return cheeseNameOnly;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     /* WORK HERE */
-    return nil;
+    // NSString *phrase = [cheese numberOfCheesesStringWithCheeseCount:4];
+    // phrase is "4 cheeses"
+    // NSString *phrase2 = [cheese numberOfCheesesStringWithCheeseCount:1];
+    // phrase2 is "1 cheese"
+    NSString *cheese;
+    NSString *cheeseNumber = [NSString stringWithFormat:@"%ld cheeses", (long)cheeseCount];
+    return cheeseNumber;
+    NSString *cheeseNumber2 = [NSString stringWithFormat:@"%ld cheese", (long)cheeseCount];
+    return cheeseNumber2;
 }
 
 @end
